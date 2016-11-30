33366ced58c5509090827df564199f9e5e45bd8a
Jean Ferrerira da Silva <jean.pfs2@gmail.com>
2016-11-30 16:25:25 -0200

    first commit

---
+++ src/PackageNameServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+namespace :package_name;
+
+use Illuminate\Support\ServiceProvider;
+
+class :package_nameServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        // Bootstrap code here.
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+    }
+}
